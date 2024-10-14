21b2b4ad60b2bf4eae808f44496e8f2934e7414d
Adham <adham00109@gmail.com>
2024-10-14 18:14:18 +0800

    fix error

--- lib/utils.ts
+++ lib/utils.ts
@@ -92,7 +92,8 @@ export const debounce = (func: (...args: any[]) => void, delay: number) => {
     let timeoutId: NodeJS.Timeout | null;
     return (...args: any[]) => {
         if (timeoutId) clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => func.apply(null, args), delay);
+        //timeoutId = setTimeout(() => func.apply(null, args), delay);
+        timeoutId = setTimeout(() => func(...args), delay); // Use spread operator here
     };
 };
 
